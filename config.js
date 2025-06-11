aa38fa48658628a3b68a2717a6f25562dc8906cc
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-11 18:49:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,32 +4,80 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "mUQHQZhR#CXzwIL1um8sRwH0rL1krUXc31RI-O6BTXCuP7w7gW9k",
-    MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
-    MODE: process.env.MODE || "private",
-
-    AUTO_REPLY: convertToBool(process.env.AUTO_REPLY),
-    AUTO_VOICE: convertToBool(process.env.AUTO_VOICE),
-    AUTO_STICKER: convertToBool(process.env.AUTO_STICKER),
-    AUTO_READ_STATUS: convertToBool(process.env.AUTO_READ_STATUS),
-    AUTO_REACT_STATUS: convertToBool(process.env.AUTO_REACT_STATUS),
-    ALWAYS_OFFLINE: convertToBool(process.env.ALWAYS_OFFLINE, 'false'),// default 'false' fix
-    AUTO_TYPING: convertToBool(process.env.AUTO_TYPING),
-    READ_MESSAGE: convertToBool(process.env.READ_MESSAGE),
-    RECORDING: convertToBool(process.env.RECORDING),
-    READ_COMMAND: convertToBool(process.env.READ_COMMAND),
-    AUTO_REACT: convertToBool(process.env.AUTO_REACT),
-    ANTI_LINK: convertToBool(process.env.ANTI_LINK, 'false'),
-    ANTI_DELETE: convertToBool(process.env.ANTI_DELETE, 'false'),
-    ANTI_CALL: convertToBool(process.env.ANTI_CALL, 'false'),
-    ANTI_BAD: convertToBool(process.env.ANTI_BAD, 'false'),
-    ANTI_BOT: convertToBool(process.env.ANTI_BOT, 'false'),
-    AUTO_BLOCK: convertToBool(process.env.AUTO_BLOCK, 'false'),
-    BAD_NO_BLOCK: convertToBool(process.env.BAD_NO_BLOCK, 'false'),
-    AI_CHAT_BOT: convertToBool(process.env.AI_CHAT_BOT, 'false'),
-    AUTO_NEWS_SENDER: convertToBool(process.env.AUTO_NEWS_SENDER, 'false'),
-    AUTO_TIKTOK_SENDER: convertToBool(process.env.AUTO_TIKTOK_SENDER, 'false'),
-    AI_REPLY: convertToBool(process.env.AI_REPLY, 'false'),
-}
+SESSION_ID: process.env.SESSION_ID || "mUQHQZhR#CXzwIL1um8sRwH0rL1krUXc31RI-O6BTXCuP7w7gW9k",",
+MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
+// add your Session Id 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto reply on status 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY KHAN-MD 🤍*",
+// set the auto reply massage on status reply  
+ANTI_DELETE: process.env.ANTI_DELETE || "true",
+// set true false for anti delete     
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox", 
+// change it to 'same' if you want to resend deleted message in same chat     
+WELCOME: process.env.WELCOME || "true",
+// true if want welcome and goodbye msg in groups    
+ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+// make true to know who dismiss or promoted a member in group
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
+// make true if want auto voice reply if someone menetion you 
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/7zfdcq.jpg",
+// add custom menu and mention reply image url
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot   
+BOT_NAME: process.env.BOT_NAME || "KHAN-MD",
+// add bot namw here for menu
+STICKER_NAME: process.env.STICKER_NAME || "KHAN-MD",
+// type sticker pack name 
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react    
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+// chose custom react emojis by yourself 
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "92342758XXXX",
+// add your bot owner number
+OWNER_NAME: process.env.OWNER_NAME || "Jᴀᴡᴀᴅ TᴇᴄʜX",
+// add bot owner name
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX*",
+// add bot owner name    
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/149k8x.jpg",
+// add img for alive msg
+LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *KHAN-MD*⚡",
+// add alive msg here 
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words  
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+// make anti link true,false for groups 
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+// maks true for always online 
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+DEV: process.env.DEV || "923427582273",
+//replace with your whatsapp number        
+ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+// make it true for auto recoding 
+};
