b4d0749e26d15dd00efbdbb99d44499a5e963ecb
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-12 12:26:01 +0530

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *Random Dog Image* 🐶\n> ©Powerd By DILSHAN-MD☀' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '🐶 *Random Dog Image* 🐶\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅɪʟꜱʜᴀɴ ᴍᴅ' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error fetching dog image: ${e.message}`);
