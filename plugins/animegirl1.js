a0da2bf389fa3dca1b67c459000b6305837c71f1
Dilshan099987 <ashinsadilshan494@gmail.com>
2025-06-12 12:04:18 +0530

    Update animegirl1.js

--- plugins/animegirl1.js
+++ plugins/animegirl1.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *DILSHAN-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SPowerd By DILSHAN-MD BY Dilshan Ashinsa*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *DILSHAN-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n > ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅɪʟꜱʜᴀɴ ᴍᴅ' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
